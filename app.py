5d3525601ab62eaef02d8352362cd20bee5eaa9d
yoichi kurokawa <yoichi.kk@gmail.com>
2019-09-25 12:02:51 +0900

    'firstcomm'

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, requests
+#from tensorflow.keras.datasets import mnist
+import requests
+#import request
+import json
+import jsonify
+import numpy as np
+
+
+#import io 
+#import urllib.request
+#from PIL import Image
+
+# 画像のURL 今回はPythonのロゴを使用
+#url = "https://www.python.org/static/img/python-logo@2x.png"
+# 画像データを取得する
+#img_in = urllib.request.urlopen(url).read()
+#img_bin = io.BytesIO(img_in)
+# Pillowで開き、画像を保存する
+#img = Image.open(img_bin)
+#img.save("logo.png","PNG")
+# 以下のコメントを外すとバイナリデータそのものを確認できる
+# print(img_bin.getvalue())
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def sample():
+    #name = 'heroku practice1'
+    return render_template(template('index.html')
+
+@app.route('/index2', methods=["POST"])
+def sample2():
+    x_test = img
+    ximg_rows, img_cols = 28, 28
+
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.astype('float32')
+    x_test /= 255
+
+    test = x_test
+    test = (np.epand_dims(test,0))
+
+    data = {
+        'images': test.tolist()
+    }
+
+
+    API_URL = 'https://root67.herokuapp.com/'
+    res = requests.post(API_URL, json=data)
+
+    result = []
+    for v in res :
+        try:
+            js = json.loads(v)
+            result.append( js['data'] )
+        except Exception:
+            pass
+
+    result = int(result[0]['prediction'][0])
+    #print("もしかしたら：",result,"？かも")
+
+    return render_template("index2. html", result = result)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
