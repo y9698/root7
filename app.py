03b7face4edf216817babdb58a0b56324b75485b
yoichi kurokawa <yoichi.kk@gmail.com>
2019-09-25 16:48:58 +0900

    '8th'

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def sample():
 
 @app.route('/index2', methods=["POST"])
 def sample2():
-    x_test = request.form['img_file']
+    x_test = request.form['img']
 
     x_test = io.BytesIO(x_test)
     # Pillowで開き、画像を保存する
